e089382445adfa7b0257591d5c1387cccf9e08ac
artyomgonc-cfu <119576824+artyomgonc-cfu@users.noreply.github.com>
2023-02-28 11:52:42 +0300

    Create Lab1.pl

---
+++ Lab1.pl
@@ -0,0 +1,31 @@
+minpos(A,B,X) :-
+	A =< 0,
+    B =< 0,
+    X is -9999,
+    !.
+minpos(A,B,X) :-
+	A =< 0,
+    X is B,
+    !.
+minpos(A,B,X) :-
+	B =< 0,
+    X is A,
+    !.
+minpos(A,B,X) :-
+	A > B,
+    X is B,
+    !.
+minpos(A,B,X):-
+	A=<B,
+    X is A,
+    !.
+    
+main :-
+	read(A),
+    read(B),
+    read(C),
+    minpos(A,B,X),
+    minpos(X,C,Ans),
+    (Ans == -9999 -> writeln("Error") ; writeln(Ans)).
+	
+?- main
