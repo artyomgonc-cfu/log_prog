feaee4101a64e6cf68eb2141213451984eb8c503
artyomgonc-cfu <119576824+artyomgonc-cfu@users.noreply.github.com>
2023-04-05 17:40:48 +0300

    Create Lab5.pl

---
+++ Lab5.pl
@@ -0,0 +1,14 @@
+person(name("Murray", "Perse", "Pressley"), date(31, 11, 1995)).
+person(name("Will", "Edward", "Ayers"), date(26, 12, 1987)).
+person(name("Quincey", "Steven", "Haggard"), date(21, 3, 1994)).
+person(name("Eustace", "Elroy", "Jordan"), date(18, 8, 2001)).
+
+person(name("River", "Rosemary", "Dobson"), date(8, 3, 1996)).
+person(name("Jasmyn", "Emmie", "Wragge"), date(9, 12, 2002)).
+person(name("Lou", "Josepha", "Trueman"), date(2, 6, 1993)).
+person(name("Deidra", "Tricia", "Dudley"), date(22, 11, 1994)).
+
+spouse(name("Murray", "Perse", "Pressley"), name("River", "Rosemary", "Dobson")).
+spouse(name("Will", "Edward", "Ayers"), name("Jasmyn", "Emmie", "Wragge")).
+spouse(name("Quincey", "Steven", "Haggard"), name("Lou", "Josepha", "Trueman")).
+spouse(name("Eustace", "Elroy", "Jordan"), name("Deidra", "Tricia", "Dudley")).
