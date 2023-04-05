11b5601e67ede7c6089a7539d22179c548d9120b
artyomgonc-cfu <119576824+artyomgonc-cfu@users.noreply.github.com>
2023-04-05 19:40:57 +0300

    Update Lab5.pl

--- Lab5.pl
+++ Lab5.pl
@@ -12,3 +12,70 @@ spouse(name("Murray", "Perse", "Pressley"), name("River", "Rosemary", "Dobson"))
 spouse(name("Will", "Edward", "Ayers"), name("Jasmyn", "Emmie", "Wragge")).
 spouse(name("Quincey", "Steven", "Haggard"), name("Lou", "Josepha", "Trueman")).
 spouse(name("Eustace", "Elroy", "Jordan"), name("Deidra", "Tricia", "Dudley")).
+
+initial_list(P, S) :-
+    findall(person(Name, Date), person(Name, Date), Persons),
+    findall(spouse(Name1, Name2), spouse(Name1, Name2), Spouses),
+    append([], Persons, P),
+    append([], Spouses, S).
+
+try_name(person(Name, date(_, Mon, _)), TryName, AskedMon) :-
+    Name = TryName,
+    AskedMon is Mon, !.
+
+try_name(P, Name, AskedMon) :-
+    AskedMon is 0.
+
+mon([], _, 1).
+
+mon([P|Persons], Name, Mon) :-
+    try_name(P, Name, AskedMon),
+    AskedMon = 0,
+    mon(Persons, Name, Mon), !.
+
+mon([P|Persons], Name, AskedMon) :-
+    try_name(P, Name, AskedMon).
+
+same_mon(Persons, spouse(Name1, Name2)) :-
+    mon(Persons, Name1, Mon1),
+    mon(Persons, Name2, Mon2),
+    Mon1 = Mon2.
+
+ans(_, [], []) :- !.    
+
+ans(Persons, [S|Spouses], Ans) :-
+    same_mon(Persons, S),
+    ans(Persons, Spouses, Ans1),
+    append(Ans1, S, Ans), !.
+
+ans(Persons, [S|Spouses], Ans) :-
+    ans(Persons, Spouses, Ans),
+
+work :-
+    initial_list(P,S), ans(P, S, Ans), print(Ans).
+
+same_mon(H, W) :-
+    spouse(H, W), person(H, date(_, D, _)), person(W, date(_, D, _)).
+
+new_list(List) :-
+    findall(spouse(Name1, Name2), same_mon(Name1, Name2), List).
+
+
+
+add_person(y) :-
+    write("Enter First name: "),
+    read(FirstName),
+    write("Enter Middle name: "),
+    read(MiddleName),
+    write("Enter Last name: "),
+    read(LastName),
+    write("Enter BD day: "),
+    read(Bday),
+    write("Enter BD mon (number): "),
+    read(Bmon),
+    write("Enter BD year: "),
+    read(Byear),
+    assertz(person(name(FirstName, MiddleName, LastName), date(Bday, Bmon, Byear))),
+    write("Continue? (y/n): "),
+    read(val),
+    add_person(val).
