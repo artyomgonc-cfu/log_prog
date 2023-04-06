7330c0e33072cee01ebbf6dc3d25fa1036c36b0f
artyomgonc-cfu <119576824+artyomgonc-cfu@users.noreply.github.com>
2023-04-06 16:16:02 +0300

    Update Lab5.pl

--- Lab5.pl
+++ Lab5.pl
@@ -1,20 +1,20 @@
-person(name("Murray", "Perse", "Pressley"), date(31, 11, 1995)).
-person(name("Will", "Edward", "Ayers"), date(26, 12, 1987)).
-person(name("Quincey", "Steven", "Haggard"), date(21, 3, 1994)).
-person(name("Eustace", "Elroy", "Jordan"), date(18, 8, 2001)).
-person(name("Craig", "Madison", "Briggs"), date(15, 5, 1983)).
+person(name(murray, perse, pressley), date(31, 11, 1995)).
+person(name(will, edward, ayers), date(26, 12, 1987)).
+person(name(quincey, steven, haggard), date(21, 3, 1994)).
+person(name(eustace, elroy, jordan), date(18, 8, 2001)).
+person(name(craig, madison, briggs), date(15, 5, 1983)).
 
-person(name("River", "Rosemary", "Dobson"), date(8, 3, 1996)).
-person(name("Jasmyn", "Emmie", "Wragge"), date(9, 12, 2002)).
-person(name("Lou", "Josepha", "Trueman"), date(2, 6, 1993)).
-person(name("Deidra", "Tricia", "Dudley"), date(22, 11, 1994)).
-person(name("Dayna", "Tawnee", "Dwight"), date(3, 5, 2000)).
+person(name(river, rosemary, dobson), date(8, 3, 1996)).
+person(name(jasmyn, emmie, wragge), date(9, 12, 2002)).
+person(name(lou, josepha, trueman), date(2, 6, 1993)).
+person(name(deidra, tricia, dudley), date(22, 11, 1994)).
+person(name(dayna, tawnee, dwight), date(3, 5, 2000)).
 
-spouse(name("Murray", "Perse", "Pressley"), name("River", "Rosemary", "Dobson")).
-spouse(name("Will", "Edward", "Ayers"), name("Jasmyn", "Emmie", "Wragge")).
-spouse(name("Quincey", "Steven", "Haggard"), name("Lou", "Josepha", "Trueman")).
-spouse(name("Eustace", "Elroy", "Jordan"), name("Deidra", "Tricia", "Dudley")).
-spouse(name("Craig", "Madison", "Briggs"), name("Dayna", "Tawnee", "Dwight")).
+spouse(name(murray, perse, pressley), name(river, rosemary, dobson)).
+spouse(name(will, edward, ayers), name(jasmyn, emmie, wragge)).
+spouse(name(quincey, steven, haggard), name(lou, josepha, trueman)).
+spouse(name(eustace, elroy, jordan), name(deidra, tricia, dudley)).
+spouse(name(craig, madison, briggs), name(dayna, tawnee, dwight)).
 
 initial_list(P, S) :-
     findall(person(Name, Date), person(Name, Date), Persons),
@@ -55,24 +55,24 @@ ans(Persons, [_|Spouses], Ans) :-
     ans(Persons, Spouses, Ans).
 
 work :-
+    write("Please, do not use uppercase!"), nl,
     initial_list(P,S),
-    add_q(P, S, Op, Os),
-    print(Op), print(Os),
-    ans(Op, Os, Ans),
-    print(Ans).
+    add_q(Op, Os),
+    append(P, Op, P1),
+    append(S, Os, S1),
+    ans(P1, S1, Ans),
+    printlist(Ans).
 
-add_q(P, S, Op, Os) :-
-    Op = P, Os = S,
+add_q(P, S) :-
     write("Enter anything? (y/n):"),
     read(Ch),
     Ch = y,
-   	add_obj(y, Op, Os, Nop, Nos),
-    Op is Nop, Os is Nos,
+   	add_obj(y, P, S),
     !.
 
-add_q(P, S, P, S) :- !.
+add_q([], []) :- !.
 
-add_obj(y, Op, Os, Nop, Nos) :-
+add_obj(y, [Pobj|P], S) :-
     write("Enter person or spouse? (p/s):"),
     read(Ch),
     
@@ -91,14 +91,13 @@ add_obj(y, Op, Os, Nop, Nos) :-
     write("Enter BD year: "),
     read(Byear),
 
-    append(Op, [person(name(FirstName, MiddleName, LastName), date(Bday, Bmon, Byear))], Nop),
-
+    Pobj = person(name(FirstName, MiddleName, LastName), date(Bday, Bmon, Byear)),
+    
     write("Continue? (y/n): "),
     read(Val),
-    add_obj(Val, Nop, Os, Nop1, Nos),
-    Nop is Nop1.
+    add_obj(Val, P, S).
 
-add_obj(y, Op, Os, Nop, Nos) :-
+add_obj(y, P, [Sobj|S]) :-
 
     write("Enter husband's First name: "),
     read(FirstName1),
@@ -114,9 +113,32 @@ add_obj(y, Op, Os, Nop, Nos) :-
     write("Enter wife's Last name: "),
     read(LastName2),
 
-    append(Os, [spouse(name(FirstName1, MiddleName1, LastName1), name(FirstName2, MiddleName2, LastName2))], Nos),
+    Sobj = spouse(name(FirstName1, MiddleName1, LastName1), name(FirstName2, MiddleName2, LastName2)),
 
     write("Continue? (y/n): "),
     read(Val),
-    add_obj(Val, Op, Nos, Nop, Nos1),
-    Nos is Nos1.
+    add_obj(Val, P, S),
+    !.
+
+add_obj(_, [], []) :- !.
+
+printlist([]) :- !.
+printlist([Sobj|S]) :-
+    printobj(Sobj),
+    printlist(S).
+
+printobj(spouse(name(FirstName1, MiddleName1, LastName1), name(FirstName2, MiddleName2, LastName2))) :-
+    print(FirstName1),
+    write(" "),
+    print(MiddleName1),
+    write(" "),
+    print(LastName1),
+    write(" <-> "),
+    print(FirstName2),
+    write(" "),
+    print(MiddleName2),
+    write(" "),
+    print(LastName2),
+    nl.
+    
+% ?- work
